0fe993be5fd41d7db14a5f386077b29a4f9b08e2
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2021-06-27 01:00:18 +0100

    i wonder if this will just work lmfao

--- main.py
+++ main.py
@@ -16,20 +16,31 @@ if not os.path.exists(DEFAULT_AV):
 # Pre cache default avatar.
 with open(DEFAULT_AV, "rb") as f: CACHED_DEFAULT = f.read()
 
+using_gif = False
 async def serve_avatar(req: Request, u_id: str) -> bytes:
     """Handles all avatar requests to a.ppy.sh and serves the avatar."""
 
     # Check if we serve default avatar or user av.
-    if u_id.isnumeric() and os.path.exists(
+    if numeric := u_id.isnumeric() and os.path.exists(
         av_loc := AVATARS_FOLDER + f"{u_id}.png"
     ):
         with open(av_loc, "rb") as f: avatar = f.read()
     
+    # GIF support. Prob should be in a for loop for each filetype
+    elif numeric and os.path.exists(
+        av_loc := AVATARS_FOLDER + f"{u_id}.gif"
+    ): 
+        with open(av_loc, "rb") as f: avatar = f.read()
+        using_gif = True
+
     # Serve them the cached default av.
     else: avatar = CACHED_DEFAULT
     
     # Send header so it gets treated as image.
-    req.add_header("Content-Type", "image/png")
+    if using_gif and req.headers["User-Agent"] != "osu!":
+        req.add_header("Content-Type", "image/gif")
+    else:
+        req.add_header("Content-Type", "image/png")
     return avatar
 
 # Create the web server and assign all the routes.
